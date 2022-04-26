4ccf0e65e6e303460ad2af1a19a0718c4d802a35
DiMatveev1991 <98545906+DiMatveev1991@users.noreply.github.com>
2022-04-26 19:34:09 +0300

    12.5.1

---
+++ 12.1.5/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace unit12._1._5
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Worker worker = new Worker();
+            worker.ShowAds();
+        }
+    }
+}
---
+++ 12.1.5/User.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace unit12._1._5
+{
+    public class User
+    {
+        public string Login { get; set; }
+        public string Name { get; set; }
+        public bool IsPremium { get; set; }
+
+        public User()
+        {
+            Console.WriteLine("Введите логин:");
+            Login = Console.ReadLine();
+
+            Console.WriteLine("Введите имя:");
+            Name = Console.ReadLine();
+
+            var premiumHas = new Dictionary<string, string>()
+            {
+                {"Foma", "Dima"},
+                {"drowWaker", "Egor" },
+                {"grishq1992", "grisha" },
+            };
+            foreach (KeyValuePair<string, string> pair in premiumHas)
+            {
+                if (pair.Key==Login && pair.Value == Name)
+                {
+                    IsPremium = true;
+                    break;
+                }
+                else 
+                {
+                    IsPremium = false;
+                }
+            }
+        }
+    }
+}
+
